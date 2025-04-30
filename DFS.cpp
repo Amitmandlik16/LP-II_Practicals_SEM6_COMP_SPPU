7ee35c4bc2a541386cbcb6ff8d835e654d7aec7e
Amitmandlik16 <amitmandlik111@gmail.com>
2025-04-30 08:36:26 +0530

    Lp-II codes

--- DFS.cpp
+++ DFS.cpp
@@ -1,59 +1,96 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 class Graph
 {
-    int **adj,v,e;
+    int v,e,**adj;
     public:
-    Graph(int v,int e);
-    void newEdge(int start,int e);
-    void DFS(int start,bool* visited);
+    Graph(int vertexs,int edges);
+    void printGraph();
+    void newEdge(int start,int end);
+    void dfs(int start);
 };
- 
+
 Graph::Graph(int v,int e)
 {
     this->v=v;
     this->e=e;
     adj=new int*[v];
-    for(int row=0;row<v;row++)
-    {
-        adj[row]=new int[v];
-        for(int column=0;column<v;column++)
-        adj[row][column]=0;
-    }
-}
- 
-void Graph::newEdge(int start,int e)
-{
-    adj[start][e]=1;
-    adj[e][start]=1;
-}
- 
-void Graph::DFS(int start,bool* visited)
-{
-    cout<<start<<" ";
-    visited[start]=true;
     for(int i=0;i<v;i++)
     {
-        if(adj[start][i]==1 && (!visited[i]))
-        DFS(i,visited);
+        adj[i]=new int[v];
+        for(int j=0;j<v;j++)
+        {
+            adj[i][j]=0;
+        }
     }
 }
- 
+
+void Graph::printGraph()
+{
+    cout<<"\nPrinting the Graph"<<endl<<"  ";
+    for(int i=0;i<v;i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<"------------------------"<<endl;
+    
+    for(int i=0;i<v;i++)
+    {
+        cout<<i<<"|";
+        for(int j=0;j<v;j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void Graph::newEdge(int start,int end)
+{
+    adj[start][end]=1;
+    adj[end][start]=1;
+}
+
+void Graph::dfs(int start)
+{
+    cout<<"DFS of the graph is"<<endl;
+
+    vector<bool>visited (v,false);
+    stack<int> s;
+
+    s.push(start);
+    visited[start]=true;
+
+    while(!s.empty())
+    {
+        int a=s.top();
+        s.pop();
+        cout<<a<<" ";
+        for(int i=v-1;i>=0;i--)
+        {
+            if((adj[a][i]==1) && (visited[i]==false) )
+            {
+                s.push(i);
+                visited[i]=true;
+            }
+        }
+    }
+}
+
 int main()
 {
     int v=8,e=8;
-    Graph G(v,e);
-    G.newEdge(0,1);
-    G.newEdge(0,2);
-    G.newEdge(1,3);
-    G.newEdge(2,4);
-    G.newEdge(2,6);
-    G.newEdge(6,7);
-    G.newEdge(3,5);
-    G.newEdge(5,4);
-    bool *visited=new bool[v];
-    for(int i=0;i<v;i++)
-    visited[i]=false;
-    G.DFS(0,visited);
+    Graph g(v,e);
+    g.newEdge(0,1);
+    g.newEdge(0,2);
+    g.newEdge(1,3);
+    g.newEdge(2,4);
+    g.newEdge(2,6);
+    g.newEdge(6,7);
+    g.newEdge(3,5);
+    g.newEdge(5,4);
+    g.printGraph();
+    g.dfs(0);
 }
